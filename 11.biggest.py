4f4787a319c5b87cf3a7654c04cc6b9b557e4ee1
Adithya931 <76829988+Adithya931@users.noreply.github.com>
2021-03-04 12:41:24 +0530

    Add files via upload

---
+++ 11.biggest.py
@@ -0,0 +1,6 @@
+a = int(input("Enter first number: ")) 
+b = int(input("Enter second number: ")) 
+c = int(input("Enter third number: ")) 
+big = max(a, b, c) 
+print("Largest of", a, b, c, "is: ", big) 
+
