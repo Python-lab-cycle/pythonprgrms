b528deae83ff8453bd779b161fa8fd83a18d8c95
Adithya931 <76829988+Adithya931@users.noreply.github.com>
2021-03-04 12:25:49 +0530

    Add files via upload

---
+++ 4.occurence.py
@@ -0,0 +1,11 @@
+line = input("Enter a Line: ")
+count = {} 
+sentence = line.split() 
+for word in sentence: 
+ if word in count: 
+   count[word] += 1 
+ else: 
+   count[word] = 1 
+for k, v in count.items(): 
+ print(k, v) 
+ 
